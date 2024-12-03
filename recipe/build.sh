f8b21c9f2d1229c64c71e7b36538086e3c877890
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-02 19:30:02 -0800

    enable rattler-build

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,9 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+export CARGO_PROFILE_RELEASE_STRIP=symbols
+export CARGO_PROFILE_RELEASE_LTO=fat
+
 # check licenses
 cargo-bundle-licenses \
     --format yaml \
@@ -9,10 +12,6 @@ cargo-bundle-licenses \
 
 # build statically linked binary with Rust
 cargo install --bins --no-track --locked --root ${PREFIX} --path pueue
-
-# strip debug symbols
-"$STRIP" "$PREFIX/bin/pueue"
-"$STRIP" "$PREFIX/bin/pueued"
 
 if [[ ${build_platform} == ${target_platform} ]]; then
     mkdir -p ${PREFIX}/share/fish/vendor_completions.d
